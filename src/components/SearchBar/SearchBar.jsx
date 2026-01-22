20e8199f99576d91c8206e394ebab98e9355d341
Ruben <neburtl837@gmail.com>
2026-01-22 20:12:21 +0100

    Add Json Generator

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -8,9 +8,7 @@ function SearchBar() {
   const [sortBy, setSortBy] = useState('');
 
   const handleSearch = () => {
-    console.log(searchTerm);
-    console.log(searchUbication);
-    console.log(sortBy);
+    console.log(`Searching for: ${searchTerm}, in ${searchUbication}, sorted by ${sortBy}`);
   };
 
   const handleInputChange = (event) => {
@@ -27,20 +25,25 @@ function SearchBar() {
   };
   
 
-
-
     return (
     <div className="relative bg-[url('/ravenous-bg.jpg')] bg-cover bg-center h-[480px] w-full flex flex-col items-center justify-center mb-10 ">
       <div className="text-white relative z-10 flex flex-row gap-10 items-center mt-10  font-medium">
-        <button className=" border-b-2 border-[#C49847] px-8 h-12 rounded-md font-semibold transition mt-5 hover:scale-105 delay-20 hover:text-[#C49847] hover:border-b-3"
-        onClick={() => handleSortBy('best_match')}
-        >Best Match</button>
-        <button className=" border-b-2 border-[#C49847] px-8 h-12 rounded-md font-semibold transition mt-5 hover:scale-105 delay-20 hover:text-[#C49847] hover:border-b-3"
+        <button className={`border-b-2 border-[#C49847] px-8 h-12 rounded-md font-semibold transition mt-5 hover:scale-105 delay-20 hover:border-b-3
+        ${sortBy === 'best_match' ? 'text-white bg-[#C49847]/80' : 'text-[#C49847] border-[#C49847]'}`}
+        onClick={() => handleSortBy('best_match')} 
+        >Best Match
+        </button>
+        <button className={` border-b-2 border-[#C49847] px-8 h-12 rounded-md font-semibold transition mt-5 hover:scale-105 delay-20 hover:border-b-3
+        ${sortBy === 'rating' ? 'text-white bg-[#C49847]/80' : 'text-[#C49847] border-[#C49847]'}`}
+        
         onClick={() => handleSortBy('rating')}
-        >Highest Rated</button>
-        <button className=" border-b-2 border-[#C49847] px-8 h-12 rounded-md font-semibold transition mt-5 hover:scale-105 delay-20 hover:text-[#C49847] hover:border-b-3"
+        >Highest Rated
+        </button>
+        <button className={` border-b-2 border-[#C49847] px-8 h-12 rounded-md font-semibold transition mt-5 hover:scale-105 delay-20 hover:border-b-3
+        ${sortBy === 'review_count' ? 'text-white bg-[#C49847]/80' : 'text-[#C49847] border-[#C49847]'}`}
         onClick={() => handleSortBy('review_count')}
-        >Most Reviewed</button>
+        >Most Reviewed
+        </button>
       </div>
        <div className="relative z-10 flex flex-row gap-10 items-center mt-10 mb-5">
         <input 
@@ -60,6 +63,7 @@ function SearchBar() {
       
       </div>
       <button className="bg-[#C49847] text-white px-8 h-12 rounded-md font-medium transition shadow-md mt-5 hover:scale-105 delay-20"
+   
       onClick={handleSearch}
       >
           Let&apos;s Go
